31a2247494f927b702d09ee9de8d6e943dbb9dac
Tim <tj2904@users.noreply.github.com>
2020-07-25 10:58:42 +0100

    Picker added
    Replicates the origami game that asks a player to pick a colour then a number before revealing an instruction.

---
+++ picker.py
@@ -0,0 +1,44 @@
+# Version 1 - Prompts for a colour, then a number before giving a random direction
+import time # To be able to sleep
+import random # For number generation
+
+# Prompt for a colour - TODO: add validation
+colour = input("Please pick a Colour: ")
+print()
+# Print out the colour a letter per row, slowly
+for c in colour:
+    print (str.upper(c) + "...")
+    time.sleep(0.5)
+
+# Prompt for a number - TODO: add validation
+n = int(input("\nNow choose a number between 1 and 8: "))
+print()
+# Count out slowly
+i = 0
+for i in range(1, n+1):
+    print (i, end="")
+    time.sleep(0.2)
+    print(".", end="")
+    time.sleep(0.2)
+    print(".", end="")
+    time.sleep(0.2)
+    print(".", end="\n")
+    i =+ 1
+
+# Generate a number at random
+final = random.randrange(1,8,1)
+#print("\nThe random number is: " + str(final))
+time.sleep(0.5)
+# Build suspense
+print ("\nI will now open the flap...", end="\n\n")
+time.sleep(2.0)
+# Return the instruction
+if (final == 1) or (final == 5):
+    print(str.center("You must turn left.", 30))
+elif (final == 2) or (final == 6):
+    print(str.center("You must turn right.", 30))
+elif (final == 3) or (final == 8):
+    print(str.center("You must turn back.", 30))
+else:
+    print(str.center("You must continue ahead.", 30))
+print()
