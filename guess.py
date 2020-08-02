04fb056c536c7488462af7dcf0e3b30ad60af4aa
Tim <tj2904@users.noreply.github.com>
2020-08-02 13:28:52 +0100

    Update guess.py
    Added more comments to describe the blocks

--- guess.py
+++ guess.py
@@ -8,6 +8,7 @@ number = random.randrange(1,1001)
 guess_count = 0
 guess = 0
 
+# Prompt the user for a guess and respond with a higher or lower answer while incorrect
 while (guess != number):
     if (guess_count == 0):
         guess = int(input("Hi, wanna play a game? \nI've thought of a number between 1 and 1000, can you guess it? "))
@@ -19,6 +20,7 @@ while (guess != number):
         guess = int(input("Narh, lower than that...\nTry again: "))
         guess_count += 1
 
+# Congratulate the user when the guess is correct
 if (guess == number):
     print("\nGood work, you've guessed it!")
     print("You took " + str(guess_count) + " goes to get it.\n")
