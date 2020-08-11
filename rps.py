c9b524f017d97a7d74e58e1ccdb0c9dc88214799
Tim <tj2904@users.noreply.github.com>
2020-08-11 12:39:27 +0100

    replaced whitespace uses of print()

--- rps.py
+++ rps.py
@@ -4,29 +4,27 @@ import random
 # add above to a dict so you can replace number with words in the sentence
 moves = {1: "Rock", 2: "Paper", 3: "Scissors"}
 
-print(dict(moves))
-print()
+#print(dict(moves)) debugging
+#print()
+
 user = int(input("Please make a choice. 1= Rock, 2= Paper, 3= Scissors: "))
-print()
-print("You have selected " + moves[user])
+print("\nYou have selected " + moves[user])
 bot = random.randrange(1,3)
-print()
-print("The computer has chosen: " + moves[bot])
-print()
+print("\nThe computer has chosen: " + moves[bot])
 if (user == bot):
-    print("It's a draw!")
+    print("\nIt's a draw!")
 elif (user == 1) & (bot == 2):
-    print("Paper covers Rock.\n Computer wins, unlucky...")
+    print("\nPaper covers Rock.\n Computer wins, unlucky...")
 elif (user == 1) & (bot == 3):
-    print("Scissors are blunted by Rock.\n You win!")
+    print("\nScissors are blunted by Rock.\n You win!")
 
 elif (user == 2) & (bot == 1):
-    print("Paper covers Rock.\n You win!")
+    print("\nPaper covers Rock.\n You win!")
 elif (user == 2) & (bot == 3):
-    print("Scissors cut Paper.\n Computer wins, unlucky...")
+    print("\nScissors cut Paper.\n Computer wins, unlucky...")
 
 elif (user == 3) & (bot == 1):
-    print("Scissors are blunted by Rock.\n Computer wins, unlucky...")
+    print("\nScissors are blunted by Rock.\n Computer wins, unlucky...")
 elif (user == 3) & (bot == 2):
-    print("Scissors cut Paper.\n You win!")
-    print()
+    print("\nScissors cut Paper.\n You win!")
+print()
